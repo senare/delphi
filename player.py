be23be8a0d3b6a909dc1610a1b93b0d2dcfffda6
Manfred Nilsson <manfred.nilsson@etraveligroup.com>
2019-05-07 09:17:42 +0200

    Second Version

--- player.py
+++ player.py
@@ -1,66 +1,99 @@
 # Logic for controlling units
 
-from game import Action, is_empty, Unit, GameState, build_distance_map
 from random import randint
 
+from game import Action
+
+
 def get_actions(state):
-  switcher = {
-    0: [ Action('move', 'left'), Action('attack', 'left')   ]  ,  
-    1: [ Action('move', 'right'), Action('attack', 'right')   ]  ,  
-    2: [ Action('move', 'down'), Action('attack', 'down')   ]   ,
-    3: [ Action('move', 'up'), Action('attack', 'up')   ]  ,
-    4: [ Action('move', 'left'), Action('attack', 'left')   ]  ,  
-    5: [ Action('move', 'down'), Action('attack', 'down')   ]   ,
-    6: [ Action('move', 'right'), Action('attack', 'right')   ]  ,  
-    7: [ Action('move', 'up'), Action('attack', 'up')   ]    
-  }
+    print('### ======================================================= ###')
+    me = state.unit
 
-  print('Unit %d , %d' % (state.unit.x, state.unit.y))
-  print('FOE %d , %d' % (state.foes[0].x, state.foes[0].y))
+    trg = {
+        (me.x, me.y + 1): [Action('attack', 'down'), Action('move', 'up')],
+        (me.x, me.y - 1): [Action('attack', 'up'), Action('move', 'down')],
+        (me.x - 1, me.y): [Action('attack', 'left'), Action('move', 'right')],
+        (me.x + 1, me.y): [Action('attack', 'right'), Action('move', 'left')],
 
-  if(state.unit.x == state.units[0].x):
-    return switcher.get(randint(2,3)) 
-    
-  if(state.unit.y == state.units[0].y):
-    return switcher.get(randint(0,1)) 
+        (me.x, me.y + 2): [Action('move', 'down'), Action('attack', 'down')],
+        (me.x, me.y - 2): [Action('move', 'up'), Action('attack', 'up')],
+        (me.x - 2, me.y): [Action('move', 'left'), Action('attack', 'left')],
+        (me.x + 2, me.y): [Action('move', 'right'), Action('attack', 'right')]
+    }
 
-  if(state.unit.x == state.units[1].x):
-    return switcher.get(randint(2,3)) 
-    
-  if(state.unit.y == state.units[1].y):
-    return switcher.get(randint(0,1)) 
+    me_pos = (me.x, me.y)
+    print('My position %s' % str(me_pos))
 
-  if(state.unit.x == state.units[2].x):
-    return switcher.get(randint(2,3)) 
-    
-  if(state.unit.y == state.units[2].y):
-    return switcher.get(randint(0,1)) 
+    foes = [i for i in state.foes]
+    foes_pos = list(map(lambda f: (f.x, f.y), foes))
+    print('Foes position %s' % str(foes_pos))
 
-  if(state.unit.x == state.foes[0].x):
-    return switcher.get(randint(0,1)) 
-    
-  if(state.unit.y == state.foes[0].y):
-    return switcher.get(randint(2,3)) 
+    friends = [i for i in state.units]
+    friends_pos = list(map(lambda f: (f.x, f.y), friends))
+    print('Friends position %s' % str(friends_pos))
 
-  if(state.unit.x == state.foes[1].x):
-    return switcher.get(randint(0,1)) 
-    
-  if(state.unit.y == state.foes[1].y):
-    return switcher.get(randint(2,3)) 
+    print('Targets %s' % str(trg))
+    for f in foes_pos:
+        if f in trg:
+            move = trg.get(f)
+            print('Attack %s' % str(move))
+            return move
 
-  if(state.unit.x == state.foes[2].x):
-    return switcher.get(randint(0,1)) 
-    
-  if(state.unit.y == state.foes[2].y):
-    return switcher.get(randint(2,3)) 
+    switcher = {
+        0: [Action('move', 'left')],
+        1: [Action('move', 'right')],
+        2: [Action('move', 'down')],
+        3: [Action('move', 'up')]
+    }
 
-  return switcher.get(randint(0,3)) 
+    print('Unit %d , %d' % (state.unit.x, state.unit.y))
+    print('FOE %d , %d' % (state.foes[0].x, state.foes[0].y))
+
+    if (state.unit.x == state.units[0].x):
+        return switcher.get(randint(2, 3))
+
+    if (state.unit.y == state.units[0].y):
+        return switcher.get(randint(0, 1))
+
+    if (state.unit.x == state.units[1].x):
+        return switcher.get(randint(2, 3))
+
+    if (state.unit.y == state.units[1].y):
+        return switcher.get(randint(0, 1))
+
+    if (state.unit.x == state.units[2].x):
+        return switcher.get(randint(2, 3))
+
+    if (state.unit.y == state.units[2].y):
+        return switcher.get(randint(0, 1))
+
+    if (state.unit.x == state.foes[0].x):
+        return switcher.get(randint(0, 1))
+
+    if (state.unit.y == state.foes[0].y):
+        return switcher.get(randint(2, 3))
+
+    if (state.unit.x == state.foes[1].x):
+        return switcher.get(randint(0, 1))
+
+    if (state.unit.y == state.foes[1].y):
+        return switcher.get(randint(2, 3))
+
+    if (state.unit.x == state.foes[2].x):
+        return switcher.get(randint(0, 1))
+
+    if (state.unit.y == state.foes[2].y):
+        return switcher.get(randint(2, 3))
+
+    return switcher.get(randint(0, 3))
+
 
 def get_player_info():
-  return {
-    "id": "Delphi",
-    "name": "Delphi"
-  }
+    return {
+        "id": "Delphi",
+        "name": "Delphi"
+    }
+
 
 def game_end():
-  pass
+    pass
